5552bdc20cd7ace059ef15239364f4cac36aa3be
faeem ali <faeem.ali@gmail.com>
2024-06-21 14:02:34 +0200

    implemented basic registration and disconnect logic. Untested

--- src/broker/broker.rs
+++ src/broker/broker.rs
@@ -2,15 +2,18 @@ use std::error::Error;
 use std::io::ErrorKind::WouldBlock;
 use std::net::SocketAddr;
 use std::time::Duration;
-use log::{info, warn};
+use log::{error, info, warn};
 use serde::{Deserialize, Serialize};
 use tokio::io::{AsyncRead, AsyncReadExt, Interest};
 use tokio::net::{TcpListener, TcpStream};
-use tokio::sync::mpsc::{channel, Sender};
+use tokio::sync::mpsc::{channel, Receiver, Sender};
+use tokio::sync::mpsc::error::TryRecvError;
 use tokio::time::sleep;
 use crate::broker::auth::{authenticate, AuthMessage, MSG_TYPE_AUTH};
-use crate::broker::broker::States::{Authenticate, Register};
-use crate::broker::control::{ControlMessages, ctrl_main};
+use crate::broker::broker::Actions::{MustDisconnect, NoAction};
+use crate::broker::broker::States::{Authenticate, Process, Register};
+use crate::broker::control::{ControlMessages, ctrl_main, RegisterMessageReq};
+use crate::broker::control::ControlMessages::RegisterMessage;
 use crate::broker::protocol::{Message, Protocol};
 use crate::error::RadError;
 
@@ -23,11 +26,47 @@ pub struct BrokerConfig {
 enum States {
     Authenticate,
     Register,
+    Process,
+}
+
+#[derive(Eq, PartialEq)]
+enum Actions {
+    MustDisconnect,
+    NoAction,
+}
+
+async fn process_control_messages(conn_ctx: &mut ConnectionCtx) -> Result<Actions, Box<dyn Error + Sync + Send>> {
+    if let Some(ctrl_rx) = &mut conn_ctx.ctrl_rx {
+        match ctrl_rx.try_recv() {
+            Ok(msg) => {
+                match &msg {
+                    ControlMessages::DisconnectMessage(m) => {
+                        return Ok(MustDisconnect);
+                    }
+
+                    _ => {
+                        //ignore
+                    }
+                }
+            }
+            Err(e) => {
+                if e == TryRecvError::Empty {} else if e == TryRecvError::Disconnected {
+                    return Err(Box::new(e));
+                }
+            }
+        }
+
+        Ok(NoAction)
+    } else {
+        Err(Box::new(RadError::from("Unexpected error. ctrl_rx is None")))        
+    }
 }
 
 async fn process_messages(conn_ctx: &mut ConnectionCtx,
                           msgs: &[Message])
-                          -> Result<(), Box<dyn Error + Sync + Send>> {
+                          -> Result<bool, Box<dyn Error + Sync + Send>> {
+
+    let mut register_details_opt = None;
     loop {
         match conn_ctx.state {
             Authenticate => {
@@ -43,6 +82,7 @@ async fn process_messages(conn_ctx: &mut ConnectionCtx,
                 let auth_msg = serde_json::from_slice::<AuthMessage>(auth_msg_wrapper.body.as_slice())?;
 
                 if authenticate(&auth_msg).await? {
+                    register_details_opt = Some(auth_msg);
                     conn_ctx.state = Register;
                     continue;
                 } else {
@@ -52,34 +92,66 @@ async fn process_messages(conn_ctx: &mut ConnectionCtx,
 
             Register => {
                 //must register with the control system
+                match &register_details_opt {
+                    Some(r) => {
+                        /* create a means for the control plane to communicate back */
+                        let (conn_tx, ctrl_rx) = channel(32);
+                        let reg = RegisterMessageReq {
+                            conn_tx,
+                            name: r.name.clone(),
+                            rx_msg_types: r.rx_msg_types.clone(),
+                            tx_msg_types: r.tx_msg_types.clone(),
+                        };
+                        register_details_opt = None;
+                        conn_ctx.ctrl_rx = Some(ctrl_rx);
+
+                        conn_ctx.ctrl_tx.send(RegisterMessage(reg)).await?;
+                        conn_ctx.state = Process;
+                    }
+                    None => {
+                        return Err(Box::new(RadError::from("Failed to get register details (unexpected)")));
+                    }
+                }
+            }
+
+            Process => {
+                /* check for messages from the control plane */
+                let action = process_control_messages(conn_ctx).await?;
+                if action == MustDisconnect {
+                    let name = match &conn_ctx.name {
+                        Some(n) => {n},
+                        None => "[unknown]",
+                    };
+                    info!("Received disconnect message for {}. Disconnecting.", name);
+                    return Ok(false);
+                }
             }
         }
 
         //TODO: do this only when there's no activity
         sleep(Duration::from_millis(1)).await;
     }
-
-
-    Ok(())
 }
 
 struct ConnectionCtx {
     pub state: States,
     pub name: Option<String>,
     pub protocol: Protocol,
-    pub ctrl_tx: Sender<ControlMessages>,
+    pub ctrl_tx: Sender<ControlMessages>, //send messages from control plane
+    pub ctrl_rx: Option<Receiver<ControlMessages>>, //receive messages from the control plane
 }
 
 async fn process_connection(sock: TcpStream, addr: SocketAddr, ctrl_tx: Sender<ControlMessages>) -> Result<(), Box<dyn Error + Sync + Send>> {
     info!("broker accepted connection from: {}", addr);
 
-    let mut protocol = Protocol::new()?;
+    let protocol = Protocol::new()?;
 
     let mut conn_ctx = ConnectionCtx {
         state: Authenticate,
         name: None,
         protocol,
         ctrl_tx,
+        ctrl_rx: None,
     };
 
     let mut must_read = false;
@@ -101,12 +173,25 @@ async fn process_connection(sock: TcpStream, addr: SocketAddr, ctrl_tx: Sender<C
                         break;
                     }
                     let msgs = &conn_ctx.protocol.feed(&data);
-                    process_messages(&mut conn_ctx, &msgs).await;
+                    let process_res = process_messages(&mut conn_ctx, &msgs).await;
+                    match process_res {
+                        Ok(must_continue) => {
+                            if !must_continue {
+                                info!("Must disconnect. Aborting connecting processing");
+                                break;
+                            }
+                        }
+                        Err(e) => {
+                            error!("Error processing messages: {}", &e);
+                            break;
+                        }
+                    }
                 }
                 Err(ref e) if e.kind() == WouldBlock => {
                     continue;
                 }
                 Err(e) => {
+                    error!("Read error: {}", &e);
                     break;
                 }
             }
--- src/broker/control.rs
+++ src/broker/control.rs
@@ -1,14 +1,61 @@
+use std::collections::HashMap;
+use std::error::Error;
 use log::{info, warn};
-use tokio::sync::mpsc::Receiver;
-use crate::broker::control::ControlMessages::{DisconnectMessage, RegisterMessage};
+use tokio::sync::mpsc::{Receiver, Sender};
+use crate::broker::control::ControlMessages::DisconnectMessage;
+
+pub struct RegisterMessageReq {
+    pub name: String,
+    pub rx_msg_types: Vec<String>,
+    pub tx_msg_types: Vec<String>,
+    pub conn_tx: Sender<ControlMessages>,
+}
+
+pub struct DisconnectMessageReq;
 
 pub enum ControlMessages {
-    RegisterMessage(String),
-    DisconnectMessage(String),
+    RegisterMessage(RegisterMessageReq),
+    DisconnectMessage(DisconnectMessageReq),
+}
+
+struct CtrlCtx {
+    connections: HashMap<String, RegisterMessageReq>,
+}
+
+async fn disconnect(ctx: &mut CtrlCtx, name: &str) -> Result<(), Box<dyn Error + Sync + Send>> {
+    match ctx.connections.get(name) {
+        Some(conn) => {
+            info!("Disconnecting connection: {}", name);
+            conn.conn_tx.send(DisconnectMessage(DisconnectMessageReq{})).await?;
+        }
+        None => {
+            return Ok(());
+        }
+    }
+
+    ctx.connections.remove(name);
+
+    Ok(())
+}
+
+async fn register_connection(ctx: &mut CtrlCtx, new_conn: RegisterMessageReq) -> Result<(), Box<dyn Error + Sync + Send>>{
+    let key = new_conn.name.trim();
+    if ctx.connections.contains_key(key) {
+        /* send disconnect messages */
+        disconnect(ctx, key).await?;
+    }
+
+    info!("Registered connection: {}", &new_conn.name);
+
+    Ok(())
 }
 
 pub async fn ctrl_main(rx: Receiver<ControlMessages>) {
     info!("Broker command receiver running");
+
+    let mut ctx = CtrlCtx {
+      connections: HashMap::new(),
+    };
 
     let mut m_rx = rx;
     loop {
@@ -16,10 +63,13 @@ pub async fn ctrl_main(rx: Receiver<ControlMessages>) {
         match msg_opt {
             Some(m) => {
                 match m {
-                    RegisterMessage(s) => {
+                    ControlMessages::RegisterMessage(new_conn) => {
+                        if let Err(e) = register_connection(&mut ctx, new_conn).await {
+                            break;
+                        }
                     }
+
                     DisconnectMessage(d) => {
-                        
                     }
                 }
             }
